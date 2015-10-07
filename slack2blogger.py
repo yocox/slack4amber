ae322a1925a9b6dd677048c07a41eb723e15abe9
Xiao Keting <peter.xiau@gmail.com>
2015-10-08 03:16:22 +0800

    get all messages of all channels of specified date

--- slack2blogger.py
+++ slack2blogger.py
@@ -1,6 +1,8 @@
-#import urllib.request
-import urllib
+import urllib.request
+import urllib.parse
+#import urllib
 import json
+import datetime
 from collections import namedtuple
 
 
@@ -12,9 +14,15 @@ def url2obj(u): return json2obj(urllib.urlopen(url).read().decode())
 
 def g(api, **args):
     args['token'] = SLACK_TOKEN
-    arg_str = urllib.urlencode(args)
+    arg_str = urllib.parse.urlencode(args)
     url = 'https://slack.com/api/%s?%s' % (api, arg_str)
-    return json2obj(urllib.urlopen(url).read().decode())
+    return json2obj(urllib.request.urlopen(url).read().decode())
+
+def j(api, **args):
+    args['token'] = SLACK_TOKEN
+    arg_str = urllib.parse.urlencode(args)
+    url = 'https://slack.com/api/%s?%s' % (api, arg_str)
+    return json.loads(urllib.request.urlopen(url).read().decode())
 
 
 class User:
@@ -22,36 +30,68 @@ class User:
         self.name = name
         self.img = img
 
-def create_user_dict(user_list):
+def get_user_dict():
+    """
+    a dict {'user_id': 'user_name', ...}
+    like   {'U282834': 'amber', ...}
+    """
+    user_list = g('users.list', pretty=1)    
     result = {}
     for u in user_list.members:
         result[u.id] = User(u.name, u.profile.image_48) 
     return result
 
-def create_channel_dict(channel_list):
+def get_channel_dict():
+    """
+    a dict {'channel_id': 'chennel_name', ...}
+    like   {'C324223422': 'gossip', ...}
+    """
+    channel_list = g('channels.list')
     result = {}
     for c in channel_list.channels:
-        result[c.name] = c.id
+        result[c.id] = c.name
     return result
 
-user_list = g('users.list', pretty=1)
-user_dict = create_user_dict(user_list)
+def get_one_day_one_channel(year, month, day, channel):
+    date = datetime.date(year, month, day)
+    min_ts = datetime.datetime.combine(date, datetime.time.min).timestamp()
+    max_ts = datetime.datetime.combine(date, datetime.time.max).timestamp()
+    message_list = []
 
-channel_list = g('channels.list')
-channel_dict = create_channel_dict(channel_list)
+    while True:
+        hist = j('channels.history',
+                 channel=channel,
+                 count=1000,
+                 oldest=min_ts,
+                 latest=max_ts,
+                 inclusive=0)
 
-print 'Uesr list'
-for k, v in user_dict.iteritems():
-    print '    %20s %20s' % (k, v.name)
-print ''
+        message_list.extend(hist['messages'])
+        
+        if hist['has_more']:
+            max_ts = float(hist['messages'][-1]['ts'])
+        else:
+            break
 
-print 'Channel list'
-for k, v in channel_dict.iteritems():
-    print '    %20s %20s' % (k, v)
-print ''
+    print('Channel "%s" %d messages' % (channel, len(message_list)))
+    return message_list
 
-h = g('channels.history', channel=channel_dict['general'], count=50)
+def get_one_day(year, month, day, channel_list):
+    result = {}
+    for channel in channel_list:
+        m_list = get_one_day_one_channel(year, month, day, channel)
+        result[channel] = m_list
+    return result
 
-for m in h.messages:
-    print user_dict[m.user].name, ':', m.text
+def get_and_write_one_day(year, month, day):
+    channel_id_name_dict = get_channel_dict()
+    channel_list = channel_id_name_dict.keys()
+    fout = open('%d-%d-%d.txt' % (year, month, day), 'w', encoding='utf-8')
+    channel_messages_dict = get_one_day(year, month, day, channel_list)
+    json_str = json.dumps(channel_messages_dict, ensure_ascii=False, indent=2)
+    fout.write(json_str)
+    fout.close()
 
+user_id_name_dict = get_user_dict()
+
+get_and_write_one_day(2015, 10, 6)
